28ac0919ba202c23a3fcf8d2ae4f7fab17c50247
hvroberta89 <veberroberta@gmail.com>
2022-02-28 16:36:20 +0100

    comleted 3. task

--- angular-3/src/app/app.component.ts
+++ angular-3/src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { ConstructionService } from 'src/app/service/construction.service';
+import { Construction } from 'src/app/model/construction';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-3';
+
+  list$: Observable<Construction[]> = this.construcionService.getAll();
+
+  constructor(
+    private construcionService: ConstructionService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(construction: Construction): void {
+    this.construcionService.delete(construction.id).subscribe(
+      () => this.list$ = this.construcionService.getAll(),
+    );
+  }
 
 }
---
+++ angular-3/src/app/service/construction.service.ts
@@ -0,0 +1,44 @@
+import { Construction } from 'src/app/model/construction';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConstructionService {
+
+  apiUrl: string = environment.apiUrl;
+  entityName: string = "constructions";
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getAll(): Observable<Construction[]> {
+    return this.http.get<Construction[]>(`${ this.apiUrl }${ this.entityName }`);
+  }
+
+  get(id: number): Observable<Construction> {
+    return this.http.get<Construction>(`${ this.apiUrl }${ this.entityName }/${id}`);
+  }
+
+  create(construction: Construction): Observable<T> {
+    return this.http.post<Construction>(
+      `${this.apiUrl}${this.entityName}/${construction.id}`,
+      construction
+    );
+  }
+
+  update(construction: Construction): Observable<Construction> {
+    return this.http.patch<Construction>(
+      `${this.apiUrl}${this.entityName}/${construction.id}`,
+      construction
+    );
+  }
+
+  delete(id: number): Observable<Construction> {
+    return this.http.delete<Construction>(`${ this.apiUrl }${ this.entityName }/${id}`);
+  }
+}
