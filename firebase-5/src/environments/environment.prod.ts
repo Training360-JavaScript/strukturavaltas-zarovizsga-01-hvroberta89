51f927a6f087a3032746fafd959c764c3791669b
hvroberta89 <veberroberta@gmail.com>
2022-02-28 17:08:27 +0100

    finish

--- firebase-5/src/environments/environment.prod.ts
+++ firebase-5/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDP4V1q2KsCicvEO-hEv2jS-CXL1vuUv64",
+  authDomain: "vroberta89-firebase-5.firebaseapp.com",
+  projectId: "vroberta89-firebase-5",
+  storageBucket: "vroberta89-firebase-5.appspot.com",
+  messagingSenderId: "651707481044",
+  appId: "1:651707481044:web:279e8a70990f0609033441",
+  measurementId: "G-M0X0MFGFRV"
+};
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig
 };
--- firebase-5/src/environments/environment.ts
+++ firebase-5/src/environments/environment.ts
@@ -1,9 +1,19 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+const firebaseConfig = {
+  apiKey: "AIzaSyDP4V1q2KsCicvEO-hEv2jS-CXL1vuUv64",
+  authDomain: "vroberta89-firebase-5.firebaseapp.com",
+  projectId: "vroberta89-firebase-5",
+  storageBucket: "vroberta89-firebase-5.appspot.com",
+  messagingSenderId: "651707481044",
+  appId: "1:651707481044:web:279e8a70990f0609033441",
+  measurementId: "G-M0X0MFGFRV"
+};
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig
 };
 
 /*
