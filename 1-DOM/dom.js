0fc41bd1dcb6e9421dcb607f811a3831c54812ad
hvroberta89 <veberroberta@gmail.com>
2022-02-28 14:53:10 +0100

    completed 2. task

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,11 @@
 function manipulateDom() {
-    // 
+    const quoteElement = document.querySelectorAll('.quote');
+
+    quoteElement.forEach( item => {
+        item.style.borderColor = "blue";
+        item.style.backgroundColor = "lightblue";
+        item.style.fontStyle= "italic";
+    })
 };
 
 export { manipulateDom };
